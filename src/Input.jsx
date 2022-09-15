ec849c9fe31143d6b87f4f9c674e87e8a4696f3f
sagarkachask <sagarkacha2207@gmail.com>
2022-09-15 15:05:14 +0530

    day2 excercises some changes

--- src/Input.jsx
+++ src/Input.jsx
@@ -8,9 +8,14 @@ export default function Input({ defaultValue }) {
         inputValueChange(event.target.value)
     }
 
+    let clearValue = () => {
+        inputValueChange(defaultValue)
+    }
+
     return (
         <div>
             <input value={inputValue} onChange={inputChange}></input>
+            <button onClick={clearValue}>clear</button>
             <h3>{inputValue}</h3>
         </div>
     )
