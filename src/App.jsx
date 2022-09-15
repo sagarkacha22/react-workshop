e1433bc15c8d3d07c45a5dc791c6798fe0315c9a
sagarkachask <sagarkacha2207@gmail.com>
2022-09-15 11:22:47 +0530

    intro exercise

---
+++ src/App.jsx
@@ -0,0 +1,32 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App({name, greetings = 'Hello Again', style}) {
+
+  const h2Style = !style ? greetings.toLowerCase() === 'bye' ? {background: 'red'} : {background: 'blue'} : {background: style.background}
+
+  return (
+    <>
+      {greetings.toLowerCase().includes('hello') ? <div className='main' style={{...h2Style, height: 50}}><h3>{greetings}, {name}!</h3></div> : <div className='main' style={h2Style}><h3>{greetings}, {name}!</h3></div>}
+      
+      {/* <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div> */}
+    </>
+  );
+}
+
+export default App;
