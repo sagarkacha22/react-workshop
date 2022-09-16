233ec43d91bdcf760e9309a75b3887a160f2cba4
sagarkachask <sagarkacha2207@gmail.com>
2022-09-16 15:25:17 +0530

    ToDo

--- src/index.jsx
+++ src/index.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
+import ToDo from "./ToDo";
 // import App from "./App";
-import Toggler from "./Toogler";
-import Counter from "./Counter";
-import Input from "./Input";
+// import Toggler from "./Toogler";
+// import Counter from "./Counter";
+// import Input from "./Input";
 
 ReactDOM.render(
   <React.StrictMode>
@@ -12,14 +13,17 @@ ReactDOM.render(
     <App greetings = 'Hi' name='Jatin' />
     <App greetings = 'Bye' name='Sagar' style={{background: 'green'}}/>
     <App name='Sagar' style={{background: 'yellow'}}/> */}
-    <h2>Toggler:</h2>
+    
+    {/* <h2>Toggler:</h2>
     <Toggler />
     <br /><hr /><br />
     <h2>Counter:</h2>
     <Counter />
     <br /><hr /><br />
     <h2>Input:</h2>
-    <Input defaultValue='hi'/>
+    <Input defaultValue='hi'/> */}
+
+    <ToDo />
   </React.StrictMode>,
   document.getElementById("root")
 );
