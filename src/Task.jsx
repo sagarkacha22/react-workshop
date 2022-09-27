9189c224a80791d8a72f85c0d638bf5f455776c5
sagarkachask <sagarkacha2207@gmail.com>
2022-09-27 14:23:00 +0530

    to-do app final

--- src/Task.jsx
+++ src/Task.jsx
@@ -1,7 +1,8 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import { BiEditAlt } from "react-icons/bi";
 import { AiFillDelete } from "react-icons/ai";
 import { ImMoveUp, ImMoveDown } from "react-icons/im";
+import { TiTick } from "react-icons/ti";
 import TaskContext from "./contexts";
 // import TaskContext from "./contexts";
 
@@ -29,6 +30,8 @@ import TaskContext from "./contexts";
 
 export default function Task({ task }) {
   let [taskList, updateTaskList] = useContext(TaskContext);
+  let [editMode, updateEditMode] = useState(false);
+  let [taskValue, updateTaskValue] = useState(task.task)
 
   const moveDownTask = () => {
     // console.log(taskList)
@@ -66,42 +69,60 @@ export default function Task({ task }) {
   };
 
   const editTask = () => {
-    console.log("edit");
+    updateEditMode(true);
+  };
+
+  const saveTask = () => {
+    updateEditMode(false)
+    task.task = taskValue
   };
 
   return (
     <div key={task.id} className="container">
       <div className="row">
         <div className="col">
-          <p>{task.task}</p>
+          {editMode ? (
+            <input className="form-control w-75" value={taskValue} onChange={(event) => updateTaskValue(event.target.value)} />
+          ) : (
+            <p>{task.task}</p>
+          )}
         </div>
         <div className="col d-flex flex-row-reverse">
           <button
-            className="moveDown btn btn-outline-primary me-2"
+            className="moveDown btn btn-outline-secondary me-2"
             onClick={moveDownTask}
             disabled={taskList[taskList.length - 1] === task}
           >
             <ImMoveDown />
           </button>
           <button
-            className="moveUp btn btn-outline-primary me-2"
+            className="moveUp btn btn-outline-secondary me-2"
             onClick={moveUpTask}
             disabled={taskList[0] === task}
           >
             <ImMoveUp />
           </button>
           <button
-            className="delete btn btn-outline-primary me-2"
+            className="delete btn btn-outline-danger me-2"
             onClick={deleteTask}
           >
             <AiFillDelete />
           </button>
-          <button
-            className="edit btn btn-outline-primary me-2"
-            onClick={editTask}
-          >
-            <BiEditAlt />
-          </button>
+          {editMode ? (
+            <button
+              className="edit btn btn-outline-primary me-2"
+              onClick={saveTask}
+            >
+              <TiTick />
+            </button>
+          ) : (
+            <button
+              className="edit btn btn-outline-primary me-2"
+              onClick={editTask}
+            >
+              <BiEditAlt />
+            </button>
+          )}
         </div>
       </div>
     </div>
