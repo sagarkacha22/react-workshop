3f1ba35680d5322ec88ebaa0786ba5238f2aabaf
sagarkachask <sagarkacha2207@gmail.com>
2022-09-27 13:21:30 +0530

    promises

--- src/index.jsx
+++ src/index.jsx
@@ -8,6 +8,45 @@ import ToDoApp from "./ToDoApp";
 // import Counter from "./Counter";
 // import Input from "./Input";
 
+/* promises */
+// fetch("https://jsonplaceholder.typicode.com/todos/35")
+//   .then((res) => res.json())
+//   .then((body) => console.log(body))
+//   .catch((err) => console.error(err))
+//   .then(() => {
+//     fetch("https://jsonplaceholder.typicode.com/todos", {
+//       method: "POST",
+//       headers: new Headers().append("Content-Type", "application/json"),
+//       body: JSON.stringify({
+//         title: "let's see",
+//       }),
+//     })
+//       .then((res) => res.json())
+//       .then((data) => console.log("yay! ", data))
+//       .catch((err) => console.error(err))
+//   });
+
+fetch("https://jsonplaceholder.typicode.com/todos/67")
+  .then((res) => {
+    if (!res.ok) throw new Error("something went wrong!");
+    return res.json();
+  })
+  .then((body) => {
+    let title = body.title;
+    console.log(title);
+    fetch("https://jsonplaceholder.typicode.com/todos", {
+      method: "POST",
+      headers: new Headers().append("Content-Type", "application/json"),
+      body: JSON.stringify({
+        title,
+      }),
+    })
+      .then((res) => res.json())
+      .then((data) => console.log("yay! ", data.id));
+  })
+  .catch((err) => console.error(err));
+/* promises end */
+
 ReactDOM.render(
   <React.StrictMode>
     {/* <App greetings = 'Hello' name='Sagar' />
@@ -27,7 +66,6 @@ ReactDOM.render(
     {/* <ToDo /> */}
 
     <ToDoApp />
-
   </React.StrictMode>,
   document.getElementById("root")
 );
